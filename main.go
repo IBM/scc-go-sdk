f489dc0c410f01ee5e84566e56883dbc28792532
Jason Calalang <jcalalang@ph.ibm.com>
2021-05-24 20:01:38 +0800

    add examples and update main.go

--- main.go
+++ main.go
@@ -1,47 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/IBM/go-sdk-core/v5/core"
-	"github.com/ibm-cloud-security/scc-go-sdk/posturemanagementv1"
-	"os"
+	examples "github.com/ibm-cloud-security/scc-go-sdk/examples/posturemanagementv1"
 )
 
-func CreateCollector() {
-	apiKey := os.Getenv("IAM_API_KEY")
-	url := os.Getenv("IAM_APIKEY_URL")
-	accountId := os.Getenv("ACCOUNT_ID")
-	headers := make(map[string]string)
-	headers["Content-Type"] = "application/json"
-	headers["Authorization"] = os.Getenv("OAUTH_TOKEN")
-	authenticator := &core.IamAuthenticator{
-		ApiKey: apiKey,
-		URL:    url, //use for dev/preprod env
-
-	}
-	service, _ := posturemanagementv1.NewPostureManagementV1UsingExternalConfig(&posturemanagementv1.PostureManagementV1Options{
-		Authenticator: authenticator,
-		URL:           "https://asap-dev.compliance.test.cloud.ibm.com/posture/v1/collectors?account_id=" + accountId, //Specify url or use default
-	})
-
-	source := service.NewCreateCollectorOptions(accountId)
-	source.SetCollectorName("jason-test-collector-01")
-	source.SetCollectorDescription("jason scope")
-	source.SetInstallationType("installed")
-	source.SetIsPublic(true)
-	source.SetPassphrase("secret")
-
-	result, response, err := service.CreateCollector(source)
-
-	if err != nil {
-		fmt.Println(response.Result)
-		fmt.Println("Failed to create collector: ", err)
-		return
-	}
-	fmt.Println(*result.CollectorID)
-
-}
-
 func main() {
-	CreateCollector()
+	examples.CreateCollector()
 }

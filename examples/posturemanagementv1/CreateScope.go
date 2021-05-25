b564d6cadd4d9541c0d8a54e01652227d4bffbff
Jason Calalang <jcalalang@ph.ibm.com>
2021-05-25 12:07:17 +0800

    create scope

--- examples/posturemanagementv1/CreateScope.go
+++ examples/posturemanagementv1/CreateScope.go
@@ -1,1 +1,43 @@
 package examples
+
+import (
+	"fmt"
+	"github.com/IBM/go-sdk-core/v5/core"
+	scc "github.com/ibm-cloud-security/scc-go-sdk/posturemanagementv1"
+	"os"
+)
+
+func CreateScope() {
+	apiKey := os.Getenv("IAM_API_KEY")
+	url := os.Getenv("IAM_APIKEY_URL")
+	accountId := os.Getenv("ACCOUNT_ID")
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/json"
+	headers["Authorization"] = os.Getenv("OAUTH_TOKEN")
+	authenticator := &core.IamAuthenticator{
+		ApiKey: apiKey,
+		URL:    url, //use for dev/preprod env
+
+	}
+	service, _ := scc.NewPostureManagementV1(&scc.PostureManagementV1Options{
+		Authenticator: authenticator,
+		URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+	})
+
+	source := service.NewCreateScopeOptions(accountId)
+	source.SetScopeName("sample scope 01")
+	source.SetScopeDescription("sample scope description")
+	source.SetInstallationType("installed")
+	source.SetIsPublic(true)
+	source.SetPassphrase("secret")
+
+	result, response, err := service.CreateCollector(source)
+
+	if err != nil {
+		fmt.Println(response.Result)
+		fmt.Println("Failed to create collector: ", err)
+		return
+	}
+	fmt.Println("Collector ID: ", *result.CollectorID)
+
+}

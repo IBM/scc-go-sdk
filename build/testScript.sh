94895d38e1fbc1ebe73d69bffad0512f83cb4dca
Gaurav Goswami <gaurav.goswami2@ibm.com>
2021-05-31 19:55:56 +0530

    fix(SDK): IBM Cloud SCC Go SDK

--- build/testScript.sh
+++ build/testScript.sh
@@ -3,4 +3,5 @@
 if [[ $TRAVIS_BRANCH == "main" && $TRAVIS_PULL_REQUEST == "false" ]]; then
   curl https://us-south.functions.appdomain.cloud/api/v1/web/e6b54af6-ab44-4149-a8e4-e906dcc58136/default/secadvstg-location-shift.json
   make test-int-cov
+  find . -name "*.env" -type f -delete
 fi

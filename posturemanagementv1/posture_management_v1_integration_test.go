07cdde1e1a537e10245b32665b98d146908b5bf4
Jason Calalang <jcalalang@ph.ibm.com>
2021-06-01 01:23:21 +0800

    update integration test

--- posturemanagementv1/posture_management_v1_integration_test.go
+++ posturemanagementv1/posture_management_v1_integration_test.go
@@ -17,12 +17,16 @@
 package posturemanagementv1_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/IBM/go-sdk-core/v5/core"
 	"github.com/ibm-cloud-security/scc-go-sdk/posturemanagementv1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 )
 
 /**
@@ -33,41 +37,300 @@ import (
  * The integration test will automatically skip tests if the required config file is not available.
  */
 
-var _ = Describe(`Jason test`, func() {
+var _ = Describe(`SCC test`, func() {
 
 	Describe(`Integration test`, func() {
-		It(`Create collector`, func() {
-			apiKey := os.Getenv("IAM_API_KEY")
-			url := os.Getenv("IAM_APIKEY_URL")
-			accountId := os.Getenv("ACCOUNT_ID")
-			authenticator := &core.IamAuthenticator{
-				ApiKey: apiKey,
-				URL:    url, //use for dev/preprod env
-			}
-			service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
-				Authenticator: authenticator,
-				URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+		Describe(`Create collector suite`, func() {
+			It(`Create collector`, func() {
+				apiKey := os.Getenv("IAM_API_KEY")
+				authUrl := os.Getenv("IAM_APIKEY_URL")
+				accountId := os.Getenv("ACCOUNT_ID")
+				apiUrl := os.Getenv("API_URL")
+				authenticator := &core.IamAuthenticator{
+					ApiKey: apiKey,
+					URL:    authUrl, //use for dev/preprod env
+				}
+				service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
+					Authenticator: authenticator,
+					URL:           apiUrl, //Specify url or use default
+				})
+
+				source := service.NewCreateCollectorOptions(accountId)
+				source.SetCollectorName("jason-test-collector-05")
+				source.SetCollectorDescription("jason scope")
+				source.SetManagedBy("CUSTOMER")
+				source.SetIsPublic(true)
+				source.SetPassPhrase("secret")
+
+				_, response, err := service.CreateCollector(source)
+
+				if err != nil {
+					fmt.Println(response.Result)
+					fmt.Println("Failed to create collector: ", err)
+					return
+				}
+				Expect(response.StatusCode).To(Equal(200))
 			})
+			It(`Delete collector for cleanup`, func() {
+				apiKey := os.Getenv("IAM_API_KEY")
+				url := os.Getenv("IAM_APIKEY_URL")
+				accountId := os.Getenv("ACCOUNT_ID")
+				authenticator := &core.IamAuthenticator{
+					ApiKey: apiKey,
+					URL:    url, //use for dev/preprod env
+				}
+				service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
+					Authenticator: authenticator,
+					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+				})
 
-			source := service.NewCreateCollectorOptions(accountId)
-			source.SetCollectorName("jason-test-collector-05")
-			source.SetCollectorDescription("jason scope")
-			source.SetManagedBy("CUSTOMER")
-			source.SetIsPublic(true)
-			source.SetPassPhrase("secret")
+				source := service.NewCreateCollectorOptions(accountId)
+				source.SetCollectorName("jason-test-collector-05")
+				source.SetCollectorDescription("jason scope")
+				source.SetManagedBy("CUSTOMER")
+				source.SetIsPublic(true)
+				source.SetPassPhrase("secret")
 
-			_, response, err := service.CreateCollector(source)
+				_, response, err := service.CreateCollector(source)
 
-			if err != nil {
-				fmt.Println(response.Result)
-				fmt.Println("Failed to create collector: ", err)
-				return
-			}
-			Expect(response.StatusCode).To(Equal(200))
+				if err != nil {
+					fmt.Println(response.Result)
+					fmt.Println("Failed to create collector: ", err)
+					return
+				}
+				Expect(response.StatusCode).To(Equal(200))
+			})
 		})
+		Describe(`Create scope suite`, func() {
+			It(`Create scope`, func() {
+				apiKey := os.Getenv("IAM_API_KEY")
+				url := os.Getenv("IAM_APIKEY_URL")
+				accountId := os.Getenv("ACCOUNT_ID")
+				authenticator := &core.IamAuthenticator{
+					ApiKey: apiKey,
+					URL:    url, //use for dev/preprod env
+				}
+				service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
+					Authenticator: authenticator,
+					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+				})
+
+				source := service.NewCreateCollectorOptions(accountId)
+				source.SetCollectorName("jason-test-collector-05")
+				source.SetCollectorDescription("jason scope")
+				source.SetManagedBy("CUSTOMER")
+				source.SetIsPublic(true)
+				source.SetPassPhrase("secret")
+
+				_, response, err := service.CreateCollector(source)
+
+				if err != nil {
+					fmt.Println(response.Result)
+					fmt.Println("Failed to create collector: ", err)
+					return
+				}
+				Expect(response.StatusCode).To(Equal(200))
+			})
+			It(`Delete scope for cleanup`, func() {
+				apiKey := os.Getenv("IAM_API_KEY")
+				url := os.Getenv("IAM_APIKEY_URL")
+				accountId := os.Getenv("ACCOUNT_ID")
+				authenticator := &core.IamAuthenticator{
+					ApiKey: apiKey,
+					URL:    url, //use for dev/preprod env
+				}
+				service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
+					Authenticator: authenticator,
+					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+				})
+
+				source := service.NewCreateCollectorOptions(accountId)
+				source.SetCollectorName("jason-test-collector-05")
+				source.SetCollectorDescription("jason scope")
+				source.SetManagedBy("CUSTOMER")
+				source.SetIsPublic(true)
+				source.SetPassPhrase("secret")
+
+				_, response, err := service.CreateCollector(source)
+
+				if err != nil {
+					fmt.Println(response.Result)
+					fmt.Println("Failed to create collector: ", err)
+					return
+				}
+				Expect(response.StatusCode).To(Equal(200))
+			})
+		})
+
+		Describe(`Create credential suite`, func() {
+			It(`Create credential`, func() {
+				apiKey := os.Getenv("IAM_API_KEY")
+				url := os.Getenv("IAM_APIKEY_URL")
+				accountId := os.Getenv("ACCOUNT_ID")
+				authenticator := &core.IamAuthenticator{
+					ApiKey: apiKey,
+					URL:    url, //use for dev/preprod env
+				}
+				service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
+					Authenticator: authenticator,
+					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+				})
+
+				source := service.NewCreateCollectorOptions(accountId)
+				source.SetCollectorName("jason-test-collector-05")
+				source.SetCollectorDescription("jason scope")
+				source.SetManagedBy("CUSTOMER")
+				source.SetIsPublic(true)
+				source.SetPassPhrase("secret")
+
+				_, response, err := service.CreateCollector(source)
+
+				if err != nil {
+					fmt.Println(response.Result)
+					fmt.Println("Failed to create collector: ", err)
+					return
+				}
+				Expect(response.StatusCode).To(Equal(200))
+			})
+			It(`Delete credential for cleanup`, func() {
+				apiKey := os.Getenv("IAM_API_KEY")
+				url := os.Getenv("IAM_APIKEY_URL")
+				accountId := os.Getenv("ACCOUNT_ID")
+				authenticator := &core.IamAuthenticator{
+					ApiKey: apiKey,
+					URL:    url, //use for dev/preprod env
+				}
+				service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
+					Authenticator: authenticator,
+					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+				})
+
+				source := service.NewCreateCollectorOptions(accountId)
+				source.SetCollectorName("jason-test-collector-05")
+				source.SetCollectorDescription("jason scope")
+				source.SetManagedBy("CUSTOMER")
+				source.SetIsPublic(true)
+				source.SetPassPhrase("secret")
+
+				_, response, err := service.CreateCollector(source)
+
+				if err != nil {
+					fmt.Println(response.Result)
+					fmt.Println("Failed to create collector: ", err)
+					return
+				}
+				Expect(response.StatusCode).To(Equal(200))
+			})
+		})
+
+		Describe(`Create scan`, func() {
+			It(`Create scan`, func() {
+				apiKey := os.Getenv("IAM_API_KEY")
+				url := os.Getenv("IAM_APIKEY_URL")
+				accountId := os.Getenv("ACCOUNT_ID")
+				authenticator := &core.IamAuthenticator{
+					ApiKey: apiKey,
+					URL:    url, //use for dev/preprod env
+				}
+				service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
+					Authenticator: authenticator,
+					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+				})
+
+				source := service.NewCreateCollectorOptions(accountId)
+				source.SetCollectorName("jason-test-collector-05")
+				source.SetCollectorDescription("jason scope")
+				source.SetManagedBy("CUSTOMER")
+				source.SetIsPublic(true)
+				source.SetPassPhrase("secret")
+
+				_, response, err := service.CreateCollector(source)
+
+				if err != nil {
+					fmt.Println(response.Result)
+					fmt.Println("Failed to create collector: ", err)
+					return
+				}
+				Expect(response.StatusCode).To(Equal(200))
+			})
+			It(`Delete credential for cleanup`, func() {
+				apiKey := os.Getenv("IAM_API_KEY")
+				url := os.Getenv("IAM_APIKEY_URL")
+				accountId := os.Getenv("ACCOUNT_ID")
+				authenticator := &core.IamAuthenticator{
+					ApiKey: apiKey,
+					URL:    url, //use for dev/preprod env
+				}
+				service, _ := posturemanagementv1.NewPostureManagementV1(&posturemanagementv1.PostureManagementV1Options{
+					Authenticator: authenticator,
+					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
+				})
+
+				source := service.NewCreateCollectorOptions(accountId)
+				source.SetCollectorName("jason-test-collector-05")
+				source.SetCollectorDescription("jason scope")
+				source.SetManagedBy("CUSTOMER")
+				source.SetIsPublic(true)
+				source.SetPassPhrase("secret")
+
+				_, response, err := service.CreateCollector(source)
+
+				if err != nil {
+					fmt.Println(response.Result)
+					fmt.Println("Failed to create collector: ", err)
+					return
+				}
+				Expect(response.StatusCode).To(Equal(200))
+			})
+		})
+
 	})
 })
 
 //
 // Utility functions are declared in the unit test file
 //
+
+type access struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    string `json:"expires_in"`
+	Expiration   string `json:"expiration"`
+	Scope        string `json:"scope"`
+}
+
+func getAuthToken() string {
+	url := "https://iam.test.cloud.ibm.com/identity/token"
+	method := "POST"
+
+	payload := strings.NewReader("apikey=DjsEbdqjIwuP9bfTyGATAuJ9u55dsMbVNvJ8cVWdzoxz&response_type=cloud_iam&grant_type=urn%3Aibm%3Aparams%3Aoauth%3Agrant-type%3Aapikey")
+
+	client := &http.Client{}
+	req, _ := http.NewRequest(method, url, payload)
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	accessData := access{}
+	strBody := string(body)
+	json.Unmarshal([]byte(strBody), &accessData)
+
+	return accessData.AccessToken
+}
+
+func hardDeleteScope() {
+	fmt.Println("delete scope")
+}
+
+func hardDeleteCollector() {
+
+}
+
+func hardDeleteCredential() {
+
+}

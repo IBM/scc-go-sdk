9779fa124f291e11d150976c06f965beff49ff6f
Jason Calalang <jcalalang@ph.ibm.com>
2021-06-03 11:43:57 +0800

    fix create scope bug

--- posturemanagementv1/posture_management_v1_integration_test.go
+++ posturemanagementv1/posture_management_v1_integration_test.go
@@ -18,13 +18,11 @@
 package posturemanagementv1_test
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/IBM/go-sdk-core/v5/core"
@@ -42,20 +40,22 @@ import (
  * The integration test will automatically skip tests if the required config file is not available.
  */
 
-var accessToken *string
+var (
+	accessToken    *string
+	uuidWithHyphen string
+	collectorId    *string
+	collectorIds   []string
+	credentialId   string
+	accountId      string
+	scopeId        *string
+)
 
 var _ = Describe(`SCC test`, func() {
 
-	var (
-		collectorId  *string
-		collectorIds []string
-		credentialId string
-		scopeId      *string
-	)
-	uuidWithHyphen := uuid.New().String()
+	uuidWithHyphen = uuid.New().String()
+	accountId = os.Getenv("ACCOUNT_ID")
 	apiKey := os.Getenv("IAM_API_KEY")
 	authUrl := os.Getenv("IAM_APIKEY_URL")
-	accountId := os.Getenv("ACCOUNT_ID")
 	apiUrl := os.Getenv("API_URL")
 
 	authenticator := &core.IamAuthenticator{
@@ -68,144 +68,32 @@ var _ = Describe(`SCC test`, func() {
 		URL:           apiUrl,
 	}
 
-	token := getAuthToken()
-
-	XDescribe(`Integration test`, func() {
-		Describe(`Create collector suite`, func() {
-			It(`Create collector`, func() {
-				service, _ := scc.NewPostureManagementV1(&scc.PostureManagementV1Options{
-					Authenticator: authenticator,
-					URL:           apiUrl, //Specify url or use default
-				})
-
-				source := service.NewCreateCollectorOptions(accountId)
-				source.SetCollectorName("test-" + uuidWithHyphen)
-				source.SetCollectorDescription("test collector")
-				source.SetManagedBy("customer")
-				source.SetIsPublic(true)
-				source.SetPassPhrase("secret")
-
-				reply, response, err := service.CreateCollector(source)
-				collectorId = reply.CollectorID
-
-				if err != nil {
-					fmt.Println(response.Result)
-					fmt.Println("Failed to create collector: ", err)
-					return
-				}
-				Expect(response.StatusCode).To(Equal(201))
-			})
-			It(`Delete collector for cleanup`, func() {
-				responseCode := hardDeleteCollector(collectorId)
-
-				Expect(responseCode).To(Equal(200))
-			})
-		})
-		Describe(`Create scope suite`, func() {
-			It(`Create scope`, func() {
-				service, _ := scc.NewPostureManagementV1(&scc.PostureManagementV1Options{
-					Authenticator: authenticator,
-					URL:           apiUrl, //Specify url or use default
-				})
-
-				source := service.NewCreateScopeOptions(accountId)
-				source.SetScopeName("scope-" + uuidWithHyphen)
-				source.SetScopeDescription("test scope")
-				source.SetCredentialID("5645")
-				source.SetCollectorIds([]string{"1417"})
-				source.SetEnvironmentType("ibm")
-
-				reply, response, err := service.CreateScope(source)
-				scopeId = reply.ScopeID
-
-				if err != nil {
-					fmt.Println(response.Result)
-					fmt.Println("Failed to create scope: ", err)
-					return
-				}
-				Expect(response.StatusCode).To(Equal(200))
-				Expect(reply).ToNot(BeNil())
-			})
-			It(`Delete scope for cleanup`, func() {
-				response := hardDeleteScope(scopeId)
-				Expect(response).To(Equal(200))
-
-			})
-		})
-		Describe(`Create credential suite`, func() {
-			It(`Create credential`, func() {
-				credentialPath := os.Getenv("CREDENTIAL_PATH")
-				pemPath := os.Getenv("PEM_PATH")
-
-				service, _ := scc.NewPostureManagementV1(&scc.PostureManagementV1Options{
-					Authenticator: authenticator,
-					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
-				})
-
-				credentialFile, _ := os.Open(credentialPath)
-				pemFile, _ := os.Open(pemPath)
-
-				source := service.NewCreateCredentialOptions(accountId, credentialFile)
-				source.SetPemFile(pemFile)
-
-				reply, response, err := service.CreateCredential(source)
-
-				if err != nil {
-					fmt.Println(response.Result)
-					fmt.Println("Failed to create credential: ", err)
-					return
-				}
-				Expect(response.GetStatusCode()).To(Equal(201))
-				Expect(reply).ToNot(BeNil())
-			})
-		})
-		Describe(`Create scan`, func() {
-			It(`Create scan`, func() {
-				service, _ := scc.NewPostureManagementV1(&scc.PostureManagementV1Options{
-					Authenticator: authenticator,
-					URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
-				})
-
-				source := service.NewScanSummariesOptions("1188", accountId)
-				source.SetProfileID("48")
-				source.SetGroupProfileID("1")
-
-				reply, response, err := service.ScanSummaries(source)
-
-				if err != nil {
-					fmt.Println(response.Result)
-					fmt.Println("Failed to create scan: ", err)
-					return
-				}
-				Expect(response.StatusCode).To(Equal(200))
-				Expect(reply).ToNot(BeNil())
-			})
-		})
-
-	})
-	FDescribe(`Demo`, func() {
+	Describe(`Demo`, func() {
 
 		It(`Create Collector`, func() {
+			fmt.Println(`Create Collector`)
 			collectorId = demoCreateCollector(options)
-			Expect(collectorId).ToNot(BeNil())
 		})
 		It(`Create Credential`, func() {
+			fmt.Println(`Create Credential`)
 			credentialId = demoCreateCredential(options)
-			Expect(credentialId).ToNot(BeNil())
 		})
 		It(`Create Scope`, func() {
+			fmt.Println(`Create Scope`)
 			collectorIds = append(collectorIds, *collectorId)
 			scopeId = demoCreateScope(options, credentialId, collectorIds)
-			Expect(scopeId).ToNot(BeNil())
-		})
-		It(`Discovery`, func() {
-			demoDiscovery(&token, collectorIds, *scopeId)
 		})
 		It(`List Scopes`, func() {
+			fmt.Println(`List Scopes`)
 			demoListScope(options, scopeId)
 		})
 		It(`List Profiles`, func() {
+			fmt.Println(`List Profiles`)
 			demoListProfiles(options)
+		})
+		It(`Initiate Scan Validation`, func() {
+			fmt.Println(`Create Scan`)
+			demoCreateScanValidation(options, *scopeId, "44")
 		})
 
 	})
@@ -302,46 +190,6 @@ func hardDeleteScope(scopeId *string) int {
 }
 
 func hardDeleteCredential() {
-
-}
-
-func demoDiscovery(token *string, gatewayIds []string, scopeId string) {
-
-	accountId := os.Getenv("ACCOUNT_ID")
-	authToken := *token
-	url := os.Getenv("API_URL") + "/alpha/v1.0/schemas/tldiscover"
-	method := "POST"
-
-	tld := tlDiscover{}
-	tld.DiscoveryLevel = 1
-	tld.RequestType = "TLDISCOVER"
-	tld.SchemaId, _ = strconv.Atoi(scopeId)
-
-	for _, i := range gatewayIds {
-		j, err := strconv.Atoi(i)
-		if err != nil {
-			panic(err)
-		}
-		tld.GatewayIds = append(tld.GatewayIds, j)
-	}
-
-	requestByte, _ := json.Marshal(tld)
-	requestReader := bytes.NewReader(requestByte)
-
-	client := &http.Client{}
-	req, _ := http.NewRequest(method, url, requestReader)
-
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", authToken)
-	req.Header.Add("REALM", accountId)
-	req.Header.Add("transaction-id", uuid.New().String())
-
-	res, _ := client.Do(req)
-	defer res.Body.Close()
-
-	ioutil.ReadAll(res.Body)
-
-	Expect(res.StatusCode).To(Equal(200))
 
 }
 
@@ -450,6 +298,7 @@ func demoListScope(options scc.PostureManagementV1Options, scopeId *string) {
 	Expect(scopeIdMatch).To(Equal(0))
 	Expect(reply.Scopes).ToNot(BeNil())
 }
+
 func demoListProfiles(options scc.PostureManagementV1Options) {
 	accountId := os.Getenv("ACCOUNT_ID")
 
@@ -468,7 +317,27 @@ func demoListProfiles(options scc.PostureManagementV1Options) {
 	Expect(reply.Profiles).ToNot(BeNil())
 	Expect(response.StatusCode).To(Equal(200))
 }
-func demoCreateScanValidation() {}
-func demoListScans()            {}
-func demoReadScan()             {}
-func demoGetScopeSummary()      {}
+func demoCreateScanValidation(options scc.PostureManagementV1Options, scopeId string, profileId string) {
+	accountId := os.Getenv("ACCOUNT_ID")
+	service, _ := scc.NewPostureManagementV1(&options)
+
+	source := service.NewCreateValidationOptions(accountId)
+	source.SetScopeID(scopeId)
+	source.SetProfileID(profileId)
+
+	reply, response, err := service.CreateValidation(source)
+
+	if err != nil {
+		fmt.Println(response.Result)
+		fmt.Println("Failed to create scan: ", err)
+		return
+	}
+	//fmt.Println("Status code: " + string(response.StatusCode))
+
+	Expect(response.StatusCode).To(Equal(200))
+	Expect(reply.Message).ToNot(BeNil())
+
+}
+func demoListScans()       {}
+func demoReadScan()        {}
+func demoGetScopeSummary() {}

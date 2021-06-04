55293a532f2d185e796a88606a9bafd4b1fe1b75
Jason Calalang <jcalalang@ph.ibm.com>
2021-06-05 00:29:44 +0800

    added polling logic in example

--- examples/posturemanagementv1/ListScopes.go
+++ examples/posturemanagementv1/ListScopes.go
@@ -7,10 +7,10 @@ import (
 	"os"
 )
 
-func ListScopes() {
+func ListScopes(options scc.PostureManagementV1Options, scopeName string, scopeId string) bool {
 	apiKey := os.Getenv("IAM_API_KEY")
 	url := os.Getenv("IAM_APIKEY_URL")
-	accountId := os.Getenv("ACCOUNT_ID")
+	accountId := os.Getenv("ACCOUNT_ID_POSTURE")
 	authenticator := &core.IamAuthenticator{
 		ApiKey: apiKey,
 		URL:    url, //use for dev/preprod env
@@ -21,15 +21,38 @@ func ListScopes() {
 		URL:           "https://asap-dev.compliance.test.cloud.ibm.com", //Specify url or use default
 	})
 
-	source := service.NewListScopesOptions(accountId)
+	source := service.NewListScopesOptions(accountId, scopeName)
+	source.SetName(scopeName)
 
-	_, response, err := service.ListScopes(source)
+	reply, response, err := service.ListScopes(source)
 
 	if err != nil {
 		fmt.Println(response.Result)
 		fmt.Println("Failed to create scope: ", err)
-		return
+		panic(err)
 	}
-	fmt.Println("Status Code: ", response.GetStatusCode())
+
+	for _, scope := range reply.Scopes {
+		if *scope.ScopeID == scopeId {
+			fmt.Println("scope id " + *scope.ScopeID)
+			fmt.Println("scope name " + *scope.Name)
+			if scope.Scans != nil {
+				for _, scans := range scope.Scans {
+					fmt.Println("scan id " + *scans.ScanID)
+					fmt.Println("scan status " + *scans.Status)
+					//if *scans.Status == "validation_completed" {
+					if *scans.Status == "discovery_completed" {
+						fmt.Println("discovery completed. test pass")
+						return true
+					}
+				}
+			} else {
+				fmt.Println("no scans yet")
+			}
+		}
+	}
+
+	fmt.Println("in progress, re-checking...")
+	return false
 
 }

b8ca3b53e68eaa5c66fc41d2bf5eea89b5ecf3d0
Nick Xitco <nxitco@ibm.com>
2021-06-25 12:13:39 -0500

    feat(SDK): Added admin services sdk

---
+++ adminserviceapiv1/admin_service_api_v1_examples_test.go
@@ -0,0 +1,197 @@
+// +build examples
+
+/**
+ * (C) Copyright IBM Corp. 2021.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package adminserviceapiv1_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/IBM/go-sdk-core/v5/core"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/IBM/scc-go-sdk/adminserviceapiv1"
+)
+
+//
+// This file provides an example of how to use the Admin Service API service.
+//
+// The following configuration properties are assumed to be defined:
+// ADMIN_SERVICE_API_URL=<service base url>
+// ADMIN_SERVICE_API_AUTH_TYPE=iam
+// ADMIN_SERVICE_API_APIKEY=<IAM apikey>
+// ADMIN_SERVICE_API_AUTH_URL=<IAM token service base URL - omit this if using the production environment>
+//
+// These configuration properties can be exported as environment variables, or stored
+// in a configuration file and then:
+// export IBM_CREDENTIALS_FILE=<name of configuration file>
+//
+const externalConfigFile = "../admin_service_api_v1.env"
+
+var (
+	adminServiceApiService *adminserviceapiv1.AdminServiceApiV1
+	config       map[string]string
+	configLoaded bool = false
+)
+
+func shouldSkipTest() {
+	if !configLoaded {
+		Skip("External configuration is not available, skipping tests...")
+	}
+}
+
+var _ = Describe(`AdminServiceApiV1 Examples Tests`, func() {
+	Describe(`External configuration`, func() {
+		It("Successfully load the configuration", func() {
+			var err error
+			_, err = os.Stat(externalConfigFile)
+			if err != nil {
+				Skip("External configuration file not found, skipping tests: " + err.Error())
+			}
+
+			os.Setenv("IBM_CREDENTIALS_FILE", externalConfigFile)
+			config, err = core.GetServiceProperties(adminserviceapiv1.DefaultServiceName)
+			if err != nil {
+				Skip("Error loading service properties, skipping tests: " + err.Error())
+			}
+
+			configLoaded = len(config) > 0
+		})
+	})
+
+	Describe(`Client initialization`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It("Successfully construct the service client instance", func() {
+			var err error
+
+			// begin-common
+
+			adminServiceApiServiceOptions := &adminserviceapiv1.AdminServiceApiV1Options{}
+
+			adminServiceApiService, err = adminserviceapiv1.NewAdminServiceApiV1UsingExternalConfig(adminServiceApiServiceOptions)
+
+			if err != nil {
+				panic(err)
+			}
+
+			// end-common
+
+			Expect(adminServiceApiService).ToNot(BeNil())
+		})
+	})
+
+	Describe(`AdminServiceApiV1 request examples`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`GetSettings request example`, func() {
+			fmt.Println("\nGetSettings() result:")
+			// begin-GetSettings
+
+			getSettingsOptions := adminServiceApiService.NewGetSettingsOptions(
+				"testString",
+			)
+
+			accountSettings, response, err := adminServiceApiService.GetSettings(getSettingsOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(accountSettings, "", "  ")
+			fmt.Println(string(b))
+
+			// end-GetSettings
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(accountSettings).ToNot(BeNil())
+
+		})
+		It(`PatchAccountSettings request example`, func() {
+			fmt.Println("\nPatchAccountSettings() result:")
+			// begin-PatchAccountSettings
+
+			locationIdModel := &adminserviceapiv1.LocationID{
+				ID: core.StringPtr("eu"),
+			}
+
+			patchAccountSettingsOptions := adminServiceApiService.NewPatchAccountSettingsOptions(
+				"testString",
+				locationIdModel,
+			)
+
+			accountSettings, response, err := adminServiceApiService.PatchAccountSettings(patchAccountSettingsOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(accountSettings, "", "  ")
+			fmt.Println(string(b))
+
+			// end-PatchAccountSettings
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(accountSettings).ToNot(BeNil())
+
+		})
+		It(`ListLocations request example`, func() {
+			fmt.Println("\nListLocations() result:")
+			// begin-ListLocations
+
+			listLocationsOptions := adminServiceApiService.NewListLocationsOptions()
+
+			locations, response, err := adminServiceApiService.ListLocations(listLocationsOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(locations, "", "  ")
+			fmt.Println(string(b))
+
+			// end-ListLocations
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(locations).ToNot(BeNil())
+
+		})
+		It(`GetLocation request example`, func() {
+			fmt.Println("\nGetLocation() result:")
+			// begin-GetLocation
+
+			getLocationOptions := adminServiceApiService.NewGetLocationOptions(
+				"us",
+			)
+
+			location, response, err := adminServiceApiService.GetLocation(getLocationOptions)
+			if err != nil {
+				panic(err)
+			}
+			b, _ := json.MarshalIndent(location, "", "  ")
+			fmt.Println(string(b))
+
+			// end-GetLocation
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(location).ToNot(BeNil())
+
+		})
+	})
+})
---
+++ adminserviceapiv1/admin_service_api_v1_integration_test.go
@@ -0,0 +1,177 @@
+// +build integration
+
+/**
+ * (C) Copyright IBM Corp. 2021.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package adminserviceapiv1_test
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/IBM/go-sdk-core/v5/core"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/IBM/scc-go-sdk/adminserviceapiv1"
+)
+
+/**
+ * This file contains an integration test for the adminserviceapiv1 package.
+ *
+ * Notes:
+ *
+ * The integration test will automatically skip tests if the required config file is not available.
+ */
+
+var accountID = os.Getenv("ACCOUNT_ID")
+
+var _ = Describe(`AdminServiceApiV1 Integration Tests`, func() {
+
+	const externalConfigFile = "../admin_service_api_v1.env"
+
+	var (
+		err                    error
+		adminServiceApiService *adminserviceapiv1.AdminServiceApiV1
+		serviceURL             string
+		config                 map[string]string
+	)
+
+	var shouldSkipTest = func() {
+		Skip("External configuration is not available, skipping tests...")
+	}
+
+	Describe(`External configuration`, func() {
+		It("Successfully load the configuration", func() {
+			_, err = os.Stat(externalConfigFile)
+			if err != nil {
+				Skip("External configuration file not found, skipping tests: " + err.Error())
+			}
+
+			os.Setenv("IBM_CREDENTIALS_FILE", externalConfigFile)
+			config, err = core.GetServiceProperties(adminserviceapiv1.DefaultServiceName)
+			if err != nil {
+				Skip("Error loading service properties, skipping tests: " + err.Error())
+			}
+			serviceURL = config["URL"]
+			if serviceURL == "" {
+				Skip("Unable to load service URL configuration property, skipping tests")
+			}
+
+			fmt.Printf("Service URL: %s\n", serviceURL)
+			shouldSkipTest = func() {}
+		})
+	})
+
+	Describe(`Client initialization`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It("Successfully construct the service client instance", func() {
+
+			adminServiceApiServiceOptions := &adminserviceapiv1.AdminServiceApiV1Options{}
+
+			adminServiceApiService, err = adminserviceapiv1.NewAdminServiceApiV1UsingExternalConfig(adminServiceApiServiceOptions)
+
+			Expect(err).To(BeNil())
+			Expect(adminServiceApiService).ToNot(BeNil())
+			Expect(adminServiceApiService.Service.Options.URL).To(Equal(serviceURL))
+		})
+	})
+
+	Describe(`GetSettings - View account settings`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`GetSettings(getSettingsOptions *GetSettingsOptions)`, func() {
+
+			getSettingsOptions := &adminserviceapiv1.GetSettingsOptions{
+				AccountID: &accountID,
+			}
+
+			accountSettings, response, err := adminServiceApiService.GetSettings(getSettingsOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(accountSettings).ToNot(BeNil())
+
+		})
+	})
+
+	Describe(`PatchAccountSettings - Update account settings`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`PatchAccountSettings(patchAccountSettingsOptions *PatchAccountSettingsOptions)`, func() {
+
+			locationIdModel := &adminserviceapiv1.LocationID{
+				ID: core.StringPtr("us"),
+			}
+
+			patchAccountSettingsOptions := &adminserviceapiv1.PatchAccountSettingsOptions{
+				AccountID: &accountID,
+				Location:  locationIdModel,
+			}
+
+			accountSettings, response, err := adminServiceApiService.PatchAccountSettings(patchAccountSettingsOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(accountSettings).ToNot(BeNil())
+
+		})
+	})
+
+	Describe(`ListLocations - View available locations`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`ListLocations(listLocationsOptions *ListLocationsOptions)`, func() {
+
+			listLocationsOptions := &adminserviceapiv1.ListLocationsOptions{
+			}
+
+			locations, response, err := adminServiceApiService.ListLocations(listLocationsOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(locations).ToNot(BeNil())
+
+		})
+	})
+
+	Describe(`GetLocation - View the details of a location`, func() {
+		BeforeEach(func() {
+			shouldSkipTest()
+		})
+		It(`GetLocation(getLocationOptions *GetLocationOptions)`, func() {
+
+			getLocationOptions := &adminserviceapiv1.GetLocationOptions{
+				LocationID: core.StringPtr("us"),
+			}
+
+			location, response, err := adminServiceApiService.GetLocation(getLocationOptions)
+
+			Expect(err).To(BeNil())
+			Expect(response.StatusCode).To(Equal(200))
+			Expect(location).ToNot(BeNil())
+
+		})
+	})
+})
+
+//
+// Utility functions are declared in the unit test file
+//

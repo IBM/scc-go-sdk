8afe5740229caf3481fc12fb7761bd5f814ceea0
archanaponnada <33832784+archanaponnada@users.noreply.github.com>
2021-11-29 11:33:47 +0530

    added posture_management_v2 configurations

--- build/testScript.sh
+++ build/testScript.sh
@@ -7,5 +7,6 @@ echo "${FINDINGS_ENV}" | base64 -d >> findings_v1.env
 echo "${NOTIFICATIONS_ENV}" | base64 -d >> notifications_v1.env
 echo "${CONFIGURATION_GOVERNANCE_ENV}" | base64 -d >> configuration_governance_v1.env
 echo "${POSTURE_MANAGEMENT_ENV}" | base64 -d >> posture_management_v1.env
+echo "${POSTURE_MANAGEMENT_V2_ENV}" | base64 -d >> posture_management_v2.env
 echo "${ADMIN_SERVICE_API_ENV}" | base64 -d >> admin_service_api_v1.env
 make test-int

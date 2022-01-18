c6fea82818c93543a5a552860a3718ebaea21631
tyao117 <tyao117@gmail.com>
2022-01-18 17:23:41 -0600

    took out a parameter

--- v3/adminserviceapiv1/admin_service_api_v1_integration_test.go
+++ v3/adminserviceapiv1/admin_service_api_v1_integration_test.go
@@ -134,16 +134,9 @@ var _ = Describe(`AdminServiceApiV1 Integration Tests`, func() {
 				ID: core.StringPtr("us"),
 			}
 
-			notificationsRegistrationModel := &adminserviceapiv1.NotificationsRegistration{
-				InstanceCrn:       core.StringPtr("testString"),
-				SourceName:        core.StringPtr("testString"),
-				SourceDescription: core.StringPtr("testString"),
-			}
-
 			patchAccountSettingsOptions := &adminserviceapiv1.PatchAccountSettingsOptions{
 				AccountID:          &accountID,
 				Location:           locationIdModel,
-				EventNotifications: notificationsRegistrationModel,
 			}
 
 			accountSettings, response, err := adminServiceApiService.PatchAccountSettings(patchAccountSettingsOptions)

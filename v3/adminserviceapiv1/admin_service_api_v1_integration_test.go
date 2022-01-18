10fefbf8e3230d47020d82eae9760eccf2ae3266
tyao117 <tyao117@gmail.com>
2022-01-18 12:53:23 -0600

    formatting

--- v3/adminserviceapiv1/admin_service_api_v1_integration_test.go
+++ v3/adminserviceapiv1/admin_service_api_v1_integration_test.go
@@ -141,8 +141,8 @@ var _ = Describe(`AdminServiceApiV1 Integration Tests`, func() {
 			// }
 
 			patchAccountSettingsOptions := &adminserviceapiv1.PatchAccountSettingsOptions{
-				AccountID:          &accountID,
-				Location:           locationIdModel,
+				AccountID: &accountID,
+				Location:  locationIdModel,
 				// EventNotifications: notificationsRegistrationModel,
 			}
 

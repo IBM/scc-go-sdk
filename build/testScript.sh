9f2e4d01f3ac59c220384965c35f28423305f29f
Gaurav Goswami <13791568+gary1998@users.noreply.github.com>
2022-11-15 21:59:53 +0530

    fix: disable ITs from configuration governance (#102)
    Co-authored-by: Gaurav Goswami <gaurav.goswami2@ibm.com>

--- build/testScript.sh
+++ build/testScript.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-curl https://us-south.functions.appdomain.cloud/api/v1/web/e6b54af6-ab44-4149-a8e4-e906dcc58136/default/secadvstg-location-shift.json
-echo "${CONFIGURATION_GOVERNANCE_ENV}" | base64 -d >> v4/configuration_governance_v1.env
 echo "${POSTURE_MANAGEMENT_ENV}" | base64 -d >> v4/posture_management_v1.env
 echo "${POSTURE_MANAGEMENT_V2_ENV}" | base64 -d >> v4/posture_management_v2.env
 echo "${ADMIN_SERVICE_API_ENV}" | base64 -d >> v4/admin_service_api_v1.env

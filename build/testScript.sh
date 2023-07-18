5398325edc4d10f7279761c2ef52d180ee1e67fd
ArchanaPonnada <archanaponnada@ArchanaPonnadas-MacBook-Pro.local>
2023-07-18 21:52:22 +0530

    Signed-off-by: ArchanaPonnada <archanaponnada@ArchanaPonnadas-MacBook-Pro.local>
    adding compliance v2 to testscript
    
    Signed-off-by: ArchanaPonnada <archanaponnada@ArchanaPonnadas-MacBook-Pro.local>

--- build/testScript.sh
+++ build/testScript.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 echo "${POSTURE_MANAGEMENT_ENV}" | base64 -d >> v4/posture_management_v1.env
 echo "${POSTURE_MANAGEMENT_V2_ENV}" | base64 -d >> v4/posture_management_v2.env
 echo "${ADMIN_SERVICE_API_ENV}" | base64 -d >> v4/admin_service_api_v1.env
+echo "${COMPLIANCE_V2_ENV}" | base64 -d >> v4/compliance_v2.env
 make test-int

8fb074783cebd62365f87da1426d756f20ee6c62
ArchanaPonnada <archanaponnada@ArchanaPonnadas-MacBook-Pro.local>
2023-08-09 16:54:17 +0530

    Signed-off-by: ArchanaPonnada <archanaponnada@ArchanaPonnadas-MacBook-Pro.local>
    adding env variable

--- build/testScript.sh
+++ build/testScript.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 echo "${POSTURE_MANAGEMENT_ENV}" | base64 -d >> v4/posture_management_v1.env
 echo "${POSTURE_MANAGEMENT_V2_ENV}" | base64 -d >> v4/posture_management_v2.env
 echo "${ADMIN_SERVICE_API_ENV}" | base64 -d >> v4/admin_service_api_v1.env
+echo "${SCC_ENV}" | base64 -d >> v5/security_and_compliance_center_api_v3.env
 make test-int

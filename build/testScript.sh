d4272bec8773108ae6e49111ea0f34596128d48f
Gaurav Goswami <gaurav.goswami2@ibm.com>
2023-08-09 17:05:44 +0530

    fix: using manual code to push gopages to gh-pages
    Signed-off-by: Gaurav Goswami <gaurav.goswami2@ibm.com>

--- build/testScript.sh
+++ build/testScript.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-echo "${POSTURE_MANAGEMENT_ENV}" | base64 -d >> v4/posture_management_v1.env
-echo "${POSTURE_MANAGEMENT_V2_ENV}" | base64 -d >> v4/posture_management_v2.env
-echo "${ADMIN_SERVICE_API_ENV}" | base64 -d >> v4/admin_service_api_v1.env
+# echo "${POSTURE_MANAGEMENT_ENV}" | base64 -d >> v4/posture_management_v1.env // DEPRECATED
+# echo "${POSTURE_MANAGEMENT_V2_ENV}" | base64 -d >> v4/posture_management_v2.env // DEPRECATED
+# echo "${ADMIN_SERVICE_API_ENV}" | base64 -d >> v4/admin_service_api_v1.env // DEPRECATED
 make test-int

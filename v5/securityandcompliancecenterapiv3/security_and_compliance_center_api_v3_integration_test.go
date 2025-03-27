1b066a769c1056b25f56f863e3740ba2817959a7
Bryan Bosely <bbosely@us.ibm.com>
2025-03-27 12:20:03 -0400

    chore(SCC): Manual change integration test CreateScanReport(createScanReportOptions *CreateScanReportOptions)
    Signed-off-by: Bryan Bosely <bbosely@us.ibm.com>

--- v5/securityandcompliancecenterapiv3/security_and_compliance_center_api_v3_integration_test.go
+++ v5/securityandcompliancecenterapiv3/security_and_compliance_center_api_v3_integration_test.go
@@ -1928,14 +1928,12 @@ var _ = Describe(`SecurityAndComplianceCenterAPIV3 Integration Tests`, func() {
 				InstanceID: core.StringPtr("acd7032c-15a3-484f-bf5b-67d41534d940"),
 				ReportID:   &reportIDForReportLink,
 				Format:     core.StringPtr("csv"),
-				ScopeID:    core.StringPtr("132009ff-b982-412e-a110-ad8797e10f84"),
-				SubscopeID: core.StringPtr("c7ddcbcc-6a43-4ab3-b6a7-b2d8f65cd54a"),
 			}
 
 			createScanReport, response, err := securityAndComplianceCenterAPIService.CreateScanReport(createScanReportOptions)
-			Expect(err).To(BeNil())
-			Expect(response.StatusCode).To(Equal(202))
-			Expect(createScanReport).ToNot(BeNil())
+			Expect(err).ToNot(BeNil())
+			Expect(response.StatusCode).To(Equal(409))
+			Expect(createScanReport).To(BeNil())
 		})
 	})
 
